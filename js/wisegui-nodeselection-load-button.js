c57c817ee9297d6f3e1284dc34887546cf59b6f7
Daniel Bimschas <daniel@bimschas.com>
2014-09-24 12:57:43 +0200

    Working on Save node selections in browser storage #117
    - user can now save multiple node selections and choose from individual when loading
    
    Still to do:
    
    - allow saving/loading reservation-specific configurations
    - embed functionality in different contexts (e.g., flash/reset/... controls), e.g. by embedding it into the wisegui-node-table.js instead of the reservation dialog

--- js/wisegui-nodeselection-load-button.js
+++ js/wisegui-nodeselection-load-button.js
@@ -4,13 +4,30 @@
  * #################################################################
  */
 
-var WiseGuiNodeSelectionLoadButton = function(setSelectionCallback) {
+// extend jQuery with the 'exists' function to check if selector returned empty result
+// and only do so if not done yet...
+if (!($.fn.exists)) {
+	$.fn.exists = function () {
+		return this.length !== 0;
+	}
+}
+
+var WiseGuiNodeSelectionLoadButton = function(selectionCallback) {
+	
 	var self = this;
+	
 	this.view = $('<button class="btn">Load Selection</button>');
 	this.view.bind('click', this, function(e) {
 		if (!self.view.attr('disabled')) {
-			var selections = JSON.parse(window.localStorage.getItem("wisegui.nodeselections")) || {};
-			setSelectionCallback(selections);
+			var nodeSelections = JSON.parse(window.localStorage.getItem("wisegui.nodeselections")) || {};
+			var callback = function(selected) {
+				selectionCallback(nodeSelections[selected]);
+			};
+			var dialogOptions = {
+				'title' : 'Pick a Node Selection',
+				'selectButtonLabel' : 'Load'
+			};
+			new WiseGuiOptionSelectionDialog(nodeSelections, callback, undefined, dialogOptions).show();
 		}
 	});
 	
@@ -19,6 +36,11 @@ var WiseGuiNodeSelectionLoadButton = function(setSelectionCallback) {
 			self.view.removeAttr('disabled');
 		} else {
 			self.view.attr('disabled', 'disabled');
+			self.view.tooltip({
+				title : 'No saved node selections available yet! (Saved selections are not '
+					+ 'synchronized with your account on the server but are stored in your Browsers local storage)',
+				delay : 100
+			});
 		}
 	};
 
--- js/wisegui-nodeselection-save-button.js
+++ js/wisegui-nodeselection-save-button.js
@@ -8,7 +8,17 @@ var WiseGuiNodeSelectionSaveButton = function(getSelectionCallback) {
 	this.view = $('<button class="btn">Save Selection</button>');
 	this.view.bind('click', this, function(e) {
 		var selection = getSelectionCallback();
-		window.localStorage.setItem("wisegui.nodeselections", JSON.stringify(selection));
-		$(window).trigger('wisegui-nodeselection-storage-changed');
+		var storedNodeSelections = JSON.parse(window.localStorage.getItem("wisegui.nodeselections")) || {};
+		var callback = function(name) {
+			storedNodeSelections[name] = selection;
+			window.localStorage.setItem("wisegui.nodeselections", JSON.stringify(storedNodeSelections));
+			$(window).trigger('wisegui-nodeselection-storage-changed');
+		};
+		var dialogOptions = {
+			'title' : 'Pick a name',
+			'selectButtonLabel' : 'Save',
+			'textbox' : true
+		};
+		new WiseGuiOptionSelectionDialog(storedNodeSelections, callback, undefined, dialogOptions).show();
 	});
 };
