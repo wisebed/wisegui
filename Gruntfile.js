97cf83ee0823277a7bd578bf46f2b13d6ce3b7a5
Daniel Bimschas <daniel@bimschas.com>
2014-09-26 10:37:17 +0200

    Started working on #115 (Build upon npm, depend on npm-deployed wisebed.js)
    - added Gruntfile and package.json
    - moved JS and static files to respective subdirectories

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      'build/<%= pkg.name %>.browserify.js': ['src/<%= pkg.name %>.js']
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'build/<%= pkg.name %>.browserify.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'static/', src:['**'], dest: 'dist/'}
+        ]
+      }
+    },
+	clean: ['build/','dist/','docs/'],
+    jshint: {
+      all: ['src/**/*.js']
+    },
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: 'src/',
+          outdir: 'docs/'
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['src/*.js'],
+        tasks: ['jshint','browserify','uglify']
+      },
+      static: {
+        files: ['static/**'],
+        tasks: ['copy']
+      },
+      docs: {
+        files: ['src/*.js'],
+        tasks: ['yuidoc']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+  grunt.registerTask('doc', ['yuidoc']);
+  grunt.registerTask('default', ['jshint','browserify','uglify','copy','yuidoc']);
+
+};
